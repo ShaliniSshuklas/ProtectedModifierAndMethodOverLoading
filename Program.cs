0fb8672bab3150c9fcf44b7ffca930029382cfe9
Shalini Shukla <111940610+ShaliniSshuklas@users.noreply.github.com>
2023-01-30 19:25:39 +0530

    feat: internal and protected internal implementation

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace InternalAndProtectedInternalModifier
+{
+    public class Internal
+    {
+        private int _privateId;
+        protected int _protectedId;
+        internal int _internalId;
+        protected internal int _protectedInternalId;
+        public int _publicid;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Internal obj = new Internal();
+            obj._internalId = 123;
+            obj._protectedInternalId = 123;
+            obj._publicid = 123;
+        }
+    }
+}
